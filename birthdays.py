c2b9aef96fb3fa52e5cf196cae7778c7492fe2c9
M-PETT <matteo.pettena@student.h-farm.com>
2019-11-18 08:21:02 -0500

    modificacommentiB

--- birthdays.py
+++ birthdays.py
@@ -1,3 +1,7 @@
+'''
+il mio compleanno è in aprilw
+'''
+
 birthdays = {
     'Albert Einstein': '03/14/1879',
     'Benjamin Franklin': '01/17/1706',
@@ -6,11 +10,13 @@ birthdays = {
     'Rowan Atkinson': '01/6/1955'}
 
 def print_birthdays():
+	'''cerca il compleanno di chi cerco'''
     print('Welcome to the birthday dictionary. We know the birthdays of these people:')
     for name in birthdays:
         print(name)
 
 def return_birthday(name):
+	'''ritorna il compleaano della star cercata '''
     if name in birthdays:
         print('{}\'s birthday is {}.'.format(name, birthdays[name]))
     else:
